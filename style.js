f48347f2e140c1d7feb92d90924dcc8d992b9544
alecvipa <hebrit_626@hotmail.com>
2019-11-04 22:04:30 -0600

    updated portfolio v1

---
+++ style.js
@@ -0,0 +1,67 @@
+
+
+$(document).ready(function () {
+    $("#Readme").hide();
+    $("#Readme2").hide();
+    $("#Readme3").hide();
+    $("#Readme4").hide();
+    $("#Readme5").hide();
+
+    // $(document).ready(function(){
+    //     $(".wordguess").mouseenter(function(){
+    //         $("#Readme").animate({
+    //             height: 'toggle'
+                
+    //         });
+    //     });
+    //     $(".wordguess").mouseleave(function(){
+    //         $("#Readme").animate({
+    //             height: 'toggle'
+                
+    //         });
+    //     });
+    //   });
+
+    $(".wordguess").hover(function () {
+
+        $("#Readme").animate({
+            height: 'toggle'
+            
+        });
+        
+    });
+    $(".harrypotter").hover(function () {
+
+        $("#Readme2").animate({
+            height: 'toggle'
+            
+        });
+        
+    });
+    $(".trivia").hover(function () {
+
+        $("#Readme3").animate({
+            height: 'toggle'
+            
+        });
+        
+    });
+    $(".GifTastic").hover(function () {
+
+        $("#Readme4").animate({
+            height: 'toggle'
+            
+        });
+        
+    });
+    $(".youtufy").hover(function () {
+
+        $("#Readme5").animate({
+            height: 'toggle'
+            
+        });
+        
+    });
+});
+
+
